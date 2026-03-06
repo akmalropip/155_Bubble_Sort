44e6530001922323ada97a4eec07d3d7b00c44d3
akmalropip <akmalropip@gmail.com>
2026-03-06 16:38:20 +0700

    menambahkan bubblesort

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -45,3 +45,38 @@ void display()
          cout << "Data ke - " << i << " : " << a[i] << endl;
     }
 }
+
+void bubleSort()
+{
+    cout << "\n===============================" << endl;
+    cout << "Hasil setiap Pass" << endl;
+    cout << "===============================" << endl;
+    for (int pass = 1; pass <n; pass++)
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j +1];
+                a[j + 1] = temp;
+            }
+        }
+
+        cout << "Pass ke - " << pass << ":\t";
+        for (int k = 0; k < n; k++)
+        {
+            cout << a[k] << "\t ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    input();
+    bubleSort();
+    display();
+    return 0;
+}
