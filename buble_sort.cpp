27994a4bb1e5dd037fbc25b867f1fa838b28bb8e
akmalropip <akmalropip@gmail.com>
2026-03-06 16:36:18 +0700

    memasukkan void display

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -32,3 +32,16 @@ void input()
     cin >> a[i];
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+         cout << "Data ke - " << i << " : " << a[i] << endl;
+    }
+}
